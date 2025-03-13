9310f7e69a3da2426bd64ac31a783a6b84ccca39
kirk-c-saunders <torikirksaunders@gmail.com>
2025-03-12 21:35:00 -0400

    Complete CityStateZip
    Created Seed Data script to populate CityStateZip, and corrected a column name in table definition.

--- Object Sources/tables/dataDeidentificationAddress.CityStateZip.sql
+++ Object Sources/tables/dataDeidentificationAddress.CityStateZip.sql
@@ -1,9 +1,9 @@
 CREATE TABLE [dataDeidentificationAddress].[CityStateZip]
 (
-	[CityStateZip] INT IDENTITY(1,1) NOT NULL
+	[CityStateZipId] INT IDENTITY(1,1) NOT NULL
 	,[CityName] VARCHAR(100) NOT NULL
 	,[StateName] VARCHAR(100) NOT NULL
 	,[StateAbbreviation] VARCHAR(2) NOT NULL /* Used VARCHAR not CHAR to avoid conversion issues in processing */
 	,[ZipCode] VARCHAR(5) NOT NULL
-	,CONSTRAINT [PK_DataDeidentificationAddress_CityStateZip] PRIMARY KEY ([CityStateZip] ASC)
+	,CONSTRAINT [PK_DataDeidentificationAddress_CityStateZip] PRIMARY KEY ([CityStateZipId] ASC)
 )
